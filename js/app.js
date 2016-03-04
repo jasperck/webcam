8d4a76652874deddf5c95dce36748eb5246093c5
JasperChang <jasperchang@kkbox.com>
2016-03-04 12:30:03 +0800

    Add app.js for getting user media

---
+++ js/app.js
@@ -0,0 +1,28 @@
+function app () {
+    "use strict";
+
+    bootstrap();
+
+    function bootstrap() {
+      getUserMedia();
+    };
+
+    function getUserMedia () {
+      navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+      var constraints = {audio: false, video: true};
+      var video = document.querySelector("video");
+      function successCallback(stream) {
+        window.stream = stream; // stream available to console
+        if (window.URL) {
+            video.src = window.URL.createObjectURL(stream);
+        } else {
+            video.src = stream;
+        }
+      }
+      function errorCallback(error){
+        console.log("navigator.getUserMedia error: ", error);
+      }
+      navigator.getUserMedia(constraints, successCallback, errorCallback);
+    }
+
+}
