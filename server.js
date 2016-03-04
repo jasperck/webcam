4dd23d162c8183bd90e5defd4d6c44eda4360ab5
JasperChang <jasperchang@kkbox.com>
2016-03-04 16:12:33 +0800

    Add convertDataURIToBinary method

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ const DEFAULT_PORT = 8008;
 var clients = [];
 var wsServer = new WebsocketServer({port: DEFAULT_PORT});
 
+function convertDataURIToBinary(dataURI) {
+  var BASE64_MARKER = ';base64,';
+  var base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
+  var base64 = dataURI.substring(base64Index);
+  return new Buffer(base64, 'base64'); // atob(base64) in window scope
+}
+
 wsServer.on('connection', (ws) => {
   util.log('websocket connected');
   clients.push(ws);
